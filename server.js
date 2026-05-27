fc55400984b66feca63f4a298611f61dcfabc830
kumardheeraj9311 <kumardheeraj9311@gmail.com>
2026-05-28 00:21:09 +0530

    asfasfsdf sdaf

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-//require("./firebase");
+require("./firebase");
 
 const http = require("http");
 const { Server } = require("socket.io");
