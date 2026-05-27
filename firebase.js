140f0126b5eac33719fb82c5c19915aabceaad3f
kumardheeraj9311 <kumardheeraj9311@gmail.com>
2026-05-28 00:33:57 +0530

    asfasfsdf sdafdsfdf

--- firebase.js
+++ firebase.js
@@ -1,4 +1,17 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("./serviceAccountKey.json");
-admin.initializeApp({ credential: admin.credential.cert(serviceAccount) });
+
+const serviceAccount = require(
+  "./serviceAccountKey.json",
+);
+
+if (!admin.apps.length) {
+
+  admin.initializeApp({
+    credential:
+      admin.credential.cert(
+        serviceAccount,
+      ),
+  });
+}
+
 module.exports = admin;
