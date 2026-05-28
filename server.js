1207592287d496cd8c895a490de0d19b3e828167
kumardheeraj9311 <kumardheeraj9311@gmail.com>
2026-05-28 06:22:15 +0530

    asfasfsdffg ss sss

--- server.js
+++ server.js
@@ -18,6 +18,159 @@ const io = new Server(server, {
   },
 });
 
+// =========================================
+// 🔌 SOCKET.IO CONNECTION
+// =========================================
+
+const broadcasters = {};
+
+io.on("connection", (socket) => {
+
+  console.log("Connected:", socket.id);
+
+  // START BROADCAST
+
+  socket.on("broadcaster", ({ roomId }) => {
+
+    broadcasters[roomId] = socket.id;
+
+    socket.join(roomId);
+
+    console.log(
+      "Broadcaster Started:",
+      roomId
+    );
+
+  });
+
+  // VIEWER JOIN
+
+  socket.on("viewer", ({ roomId }) => {
+
+    socket.join(roomId);
+
+    console.log(
+      "Viewer Joined:",
+      socket.id
+    );
+
+    const broadcasterId =
+      broadcasters[roomId];
+
+    if (broadcasterId) {
+
+      io.to(broadcasterId).emit("viewer", {
+        viewerId: socket.id,
+      });
+
+    } else {
+
+      io.to(socket.id).emit(
+        "broadcast-not-found"
+      );
+
+    }
+
+  });
+
+  // OFFER
+
+  socket.on("offer", ({
+    target,
+    offer,
+  }) => {
+
+    io.to(target).emit("offer", {
+      sender: socket.id,
+      offer,
+    });
+
+  });
+
+  // ANSWER
+
+  socket.on("answer", ({
+    target,
+    answer,
+  }) => {
+
+    io.to(target).emit("answer", {
+      sender: socket.id,
+      answer,
+    });
+
+  });
+
+  // ICE CANDIDATE
+
+  socket.on("candidate", ({
+    target,
+    candidate,
+  }) => {
+
+    if (target) {
+
+      io.to(target).emit("candidate", {
+        sender: socket.id,
+        candidate,
+      });
+
+    }
+
+  });
+
+  // STOP BROADCAST
+
+  socket.on("stop-broadcast", ({
+    roomId,
+  }) => {
+
+    console.log(
+      "Broadcast Stopped:",
+      roomId
+    );
+
+    io.to(roomId).emit(
+      "broadcast-stopped"
+    );
+
+    delete broadcasters[roomId];
+
+  });
+
+  // DISCONNECT
+
+  socket.on("disconnect", () => {
+
+    console.log(
+      "Disconnected:",
+      socket.id
+    );
+
+    for (let roomId in broadcasters) {
+
+      if (
+        broadcasters[roomId] === socket.id
+      ) {
+
+        io.to(roomId).emit(
+          "broadcast-stopped"
+        );
+
+        delete broadcasters[roomId];
+
+      }
+
+    }
+
+  });
+
+});
+
+// =========================================
+// 🔌 SOCKET.IO CONNECTION
+// =========================================
+
 app.use(cors());
 app.use(express.json());
 
@@ -33,8 +186,6 @@ app.use("/api/user", require("./routes/userRoutes"));
 app.use("/api/masjid", require("./routes/masjidRoutes"));
 app.use("/api/donate", require("./routes/donateRoutes"));
 
-
-require("./socket/liveAudio")(io);
 
 const PORT = process.env.PORT || 5000;
 
